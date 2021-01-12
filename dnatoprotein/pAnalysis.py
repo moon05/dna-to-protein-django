2b37316d8806df7a5c6c6a935a705a48a8c4c23c
Abdullah Al Mamun <amamun@u.rochester.edu>
2021-01-12 14:43:23 -0500

    working django version for simple fetching DNA search

---
+++ dnatoprotein/pAnalysis.py
@@ -0,0 +1,107 @@
+from Bio.Seq import Seq
+from Bio import SeqIO
+import time
+
+def KMPSearch(pat, txt): 
+	M = len(pat) 
+	N = len(txt) 
+
+	# create lps[] that will hold the longest prefix suffix  
+	# values for pattern 
+	lps = [0]*M 
+	j = 0 # index for pat[] 
+
+	# Preprocess the pattern (calculate lps[] array) 
+	computeLPSArray(pat, M, lps) 
+
+	i = 0 # index for txt[] 
+	while i < N: 
+		if pat[j] == txt[i]: 
+			i += 1
+			j += 1
+
+		if j == M: 
+			return (i-j)
+			j = lps[j-1] 
+
+		# mismatch after j matches 
+		elif i < N and pat[j] != txt[i]: 
+			# Do not match lps[0..lps[j-1]] characters, 
+			# they will match anyway 
+			if j != 0: 
+				j = lps[j-1] 
+			else: 
+				i += 1
+	return -1
+  
+def computeLPSArray(pat, M, lps): 
+	len = 0 # length of the previous longest prefix suffix 
+
+	lps[0] # lps[0] is always 0 
+	i = 1
+
+	# the loop calculates lps[i] for i = 1 to M-1 
+	while i < M: 
+		if pat[i]== pat[len]: 
+			len += 1
+			lps[i] = len
+			i += 1
+		else: 
+			# This is tricky. Consider the example. 
+			# AAACAAAA and i = 7. The idea is similar  
+			# to search step. 
+			if len != 0: 
+				len = lps[len-1] 
+
+			# Also, note that we do not increment i here 
+			else: 
+				lps[i] = 0
+				i += 1
+	return -1
+
+def check_match(small_string, source):
+	s = Seq(small_string)
+	res = KMPSearch(s, source)
+	return res
+
+genomeFiles = {
+				0: {"file_loc": "./dnatoprotein/protein_files/NC_000852.fasta", "name": "Paramecium bursaria Chlorella virus 1, complete genome" },
+				1: {"file_loc": "./dnatoprotein/protein_files/NC_007346.fasta", "name": "Emiliania huxleyi virus 86, complete genome"},
+				2: {"file_loc": "./dnatoprotein/protein_files/NC_008724.fasta", "name": "Acanthocystis turfacea Chlorella virus 1, complete genome"},
+				3: {"file_loc": "./dnatoprotein/protein_files/NC_009899.fasta", "name": "Paramecium bursaria Chlorella virus AR158, complete genome"},
+				4: {"file_loc": "./dnatoprotein/protein_files/NC_014637.fasta", "name": "Cafeteria roenbergensis virus BV-PW1, complete genome"},
+				5: {"file_loc": "./dnatoprotein/protein_files/NC_016072.fasta", "name": "Megavirus chiliensis, complete genome"},
+				6: {"file_loc": "./dnatoprotein/protein_files/NC_020104.fasta", "name": "Acanthamoeba polyphaga moumouvirus, complete genome"},
+				7: {"file_loc": "./dnatoprotein/protein_files/NC_023423.fasta", "name": "Pithovirus sibericum isolate P1084-T, complete genome"},
+				8: {"file_loc": "./dnatoprotein/protein_files/NC_023719.fasta", "name": "Bacillus phage G, complete genome"},
+				9: {"file_loc": "./dnatoprotein/protein_files/NC_027867.fasta", "name": "Mollivirus sibericum isolate P1084-T, complete genome"}
+			}
+
+genbank_1 = "./protein_files/chlorella.gb"
+
+
+
+#not taking reverse complement since
+#search string is not gonna be based on genes
+def search(dna_string):
+	for values in genomeFiles.values():
+		filename = values["file_loc"]
+		r = ""
+		for records in SeqIO.parse(filename, "fasta"):
+			print (records.id)
+			r = records.seq
+			print (type(r))
+			res = check_match(dna_string, r)
+			if res != -1:
+				print ({"match_loc": res, "name": values["name"]})
+				return {"match_loc": res, "name": values["name"]}
+			else:
+				continue 
+	return -1
+
+
+#for test
+first = "CAAATCAAATTCCTCAATCGGATCTCCTTCTCTTAGAAGAATACCCCAAATCAA"
+second = "ACTGTAATGTATTGCAATATCAGGGAATTATCCAAAAATCATAAATGTGCCGTCGGTGAGTTTATAACACCCGAGAACTATAACACTGTAACATTGAAGAAATGAAAATTCTTGTGATGCTATTTGTATGTATGTTGTATGCAATTGCCGACTGTATTGTTAAGTGCGTCGGTCGCCCGGCTCATAAAAAATATTATACTTTAAATAAAAGATAA"
+
+# print (search(first))
---
+++ dnatoprotein/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from time import sleep
+import asyncio
+from . pAnalysis import search
+import os
+import json
+
+# Create your views here.
+
+def index(request):
+	return HttpResponse("Hello World! You are at index page!")
+
+
+async def slowresp():
+	a = 1
+	await asyncio.sleep(2)
+	b = 1
+	await asyncio.sleep(2)
+	c = 2
+	await asyncio.sleep(2)
+	d = 2
+	await asyncio.sleep(2)
+	return [a, b, c, d]
+
+async def makeSearch(dna_string):
+	return search(dna_string)
+
+
+async def relax(request):
+	par = request.GET.get('q')
+	if par is None:
+		print (os.getcwd())
+		return HttpResponse("No Input")
+	elif par is "":
+		print (os.getcwd())
+		return HttpResponse("Empty String/Invalid Input")
+	else:
+		print (os.getcwd())
+		data = await makeSearch(par)
+		return HttpResponse(json.dumps(data))
+	
